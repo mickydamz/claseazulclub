df2a9d43d79b3b9f2e86b4fd19468043b9303983
Bahaa Ehab El Dien Abdelmawla <91119772+ArDapps@users.noreply.github.com>
2022-03-27 14:03:14 +0200

    Update HeadSection.js

--- components/HeadSection.js
+++ components/HeadSection.js
@@ -96,7 +96,7 @@ const HeadSection = () => {
                    <div className="sm:text-center lg:text-left">
                    <h1 className="text-4xl tracking-tight font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
                     
-                       <span className="block lg:py-3 xl:inline">embrace the exquisite </span>
+                       <span className="block lg:py-3 xl:inline">Embrace the exquisite </span>
                        <span className="block dark:text-white xl:inline">lifestyle, welcome to Clase Azul club nfts</span>
                        </h1>
                        <p className="mt-3 text-base dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0" >
