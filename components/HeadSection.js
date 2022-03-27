52007f59e97732bbbfed227e4d64b98e6cf8d9bf
Bahaa Ehab El Dien Abdelmawla <91119772+ArDapps@users.noreply.github.com>
2022-03-27 13:53:14 +0200

    Update HeadSection.js

--- components/HeadSection.js
+++ components/HeadSection.js
@@ -95,8 +95,9 @@ const HeadSection = () => {
                    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                    <div className="sm:text-center lg:text-left">
                    <h1 className="text-4xl tracking-tight font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
-                       <span className="block lg:py-3 xl:inline">Upload Your NFTs </span>
-                       <span className="block dark:text-white xl:inline">Discover, Collect, and Sell Extraordinary NFTs</span>
+                    
+                       <span className="block lg:py-3 xl:inline">embrace the exquisite </span>
+                       <span className="block dark:text-white xl:inline">lifestyle, welcome to Clase Azul club nfts</span>
                        </h1>
                        <p className="mt-3 text-base dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0" >
                        on the world's first & largest NFT marketplace,this Arabic Dapps Course that is will learn more about nfts and solidity programming language   </p>
